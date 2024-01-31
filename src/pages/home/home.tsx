5898e89ce4e79f4bc3c114437abb56b8797db9e4
nyxt4s <garaynyxmedina@gmail.com>
2024-01-31 13:18:52 -0300

    se modifica el body

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,5 @@
 
-import React, { FC, useState } from "react";
+import { FC, useState } from "react";
 import imgSvg from '../../assets/Programmer-amico 1.svg';
 
 interface homeProps {
@@ -10,20 +10,25 @@ const Home: FC<homeProps> = () => {
 
 
   return (
-    <section className="flex justify-center items-center h-screen object-center  max-w-screen-xl">
-      <div className="grid grid-cols-2 max-w-screen-lg place-items-center items-center bg-blue-500">
-        <div className="font-serif Times New Roman  grid grid-flow-row-3">
-          <h1 className="text-gray-200 fontTitle text-6xl">Imagine CLoud</h1>
-          <span>"Tu Tiempo Importa, Nuestro Tecnología
-            Simplifica: ¡Vive más, Preocúpate menos!"</span>
-          <button>Nuestro Servicio </button>
-          <button>Conocenos</button>
+
+  <section className="flex justify-center items-center  object-center  ">
+      <div className="grid grid-cols-2 grid-rows-3 gap-1">
+        <div className="">
+          <h1 className="text-gray-200 fontTitle text-6xl m-3">Imagine CLoud</h1>
         </div>
-        <div>
-          <img src={imgSvg} alt="" className="" />~
+        <div className="row-span-3">
+        <img src={imgSvg} alt="" className="rounded-full" />
+        </div>
+        <div className="row-start-2">
+        <span className="m-4">"Tu Tiempo Importa, Nuestro Tecnología
+            Simplifica: ¡Vive más, Preocúpate menos!"</span>
+        </div>
+        <div className="row-start-3 flex">
+          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Nuestro Servicio </button>
+          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Conocenos</button>
         </div>
       </div>
-      </section>
+  </section>
   );
 };
 
